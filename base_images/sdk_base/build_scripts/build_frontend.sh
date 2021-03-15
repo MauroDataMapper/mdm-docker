d33ea755ecb3d958b35c39cb91a129e5ea45d276
Oliver Freeman <oliver.freeman@bdi.ox.ac.uk>
2021-03-15 11:03:39 +0000

    IMprove the frontend build scripts with logging

--- base_images/sdk_base/build_scripts/build_frontend.sh
+++ base_images/sdk_base/build_scripts/build_frontend.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
 source "$NVM_DIR/nvm.sh"
-cd $MDM_UI_HOME
-npm clean-install
+cd $MDM_UI_HOME || exit
+
+echo 'Removing node_modules'
+rm -rf node_modules
+
+echo 'Installing dependencies'
+npm ci
+
+echo 'Building'
 ng build --prod
--- base_images/sdk_base/build_scripts/install_frontend.sh
+++ base_images/sdk_base/build_scripts/install_frontend.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
 source "$NVM_DIR/nvm.sh"
-cd $MDM_UI_HOME
+cd $MDM_UI_HOME || exit
+
+echo 'Installing node and npm'
 nvm install
+#nvm install-latest-npm # we will need to update to v7 asap
+
+echo 'Installing angular'
 npm config set user 0
 npm config set unsafe-perm true
 npm install -g @angular/cli@9.1.7
